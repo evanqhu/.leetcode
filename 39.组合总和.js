9e280884ce3643478a7522ee57ee89921b87e4b3
evanqhu <qianghu98@163.com>
2025-08-07 15:52:34 +0800

    feat: update

--- 39.组合总和.js
+++ 39.组合总和.js
@@ -16,15 +16,15 @@ const combinationSum = function (candidates, target) {
 
   // 回溯函数
   const backtrack = (path, sum, start) => {
-    // 终止条件
-    if (sum === target) {
-      result.push([...path]);
-      return;
-    }
-
     // 剪枝：如果当前的和已经大于目标值，则直接返回
     if (sum > target) {
       return; // 剪枝
+    }
+
+    // 满足条件，找到一组解，加入结果集
+    if (sum === target) {
+      result.push([...path]);
+      return;
     }
 
     /**
@@ -32,10 +32,9 @@ const combinationSum = function (candidates, target) {
      * 我们使用 startIndex 来控制 不能选前面已经用过的数字，但可以重复使用当前数字
      */
     for (let i = start; i < candidates.length; i++) {
-      path.push(candidates[i]);
-      backtrack(path, sum + candidates[i], i);
-      // 回溯的本质就是递归的逆过程，把最后一步“撤销”掉，试别的分支
-      path.pop();
+      path.push(candidates[i]);                 // 做选择
+      backtrack(path, sum + candidates[i], i);  // 递归尝试（可以重复选当前元素）
+      path.pop();                               // 撤销选择（回溯的本质就是递归的逆过程，把最后一步“撤销”掉，试别的分支）
     }
   };
 
